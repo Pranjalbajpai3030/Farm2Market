3aea445d39f3794c30bb615c24c3e85637b449f8
Ujjawal-Kantt <uk1287@srmist.edu.in>
2025-01-20 18:48:27 +0530

    bugs solved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    historyApiFallback: true,
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
