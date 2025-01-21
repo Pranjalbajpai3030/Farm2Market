d9f1b6dbf33cf0a6a1d54c121196680cac653b42
Ujjawal-Kantt <uk1287@srmist.edu.in>
2025-01-21 10:02:07 +0530

    frontend: bugs solved on sell page

--- frontend/src/pages/Sell.tsx
+++ frontend/src/pages/Sell.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Camera, Plus } from "lucide-react";
+import { Camera } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
+const navigate = useNavigate();
 // Function to upload image
 interface UploadImageResponse {
   imageUrl: string;
@@ -87,7 +89,19 @@ export default function Sell() {
     try {
       setError("");
       setLoading(true);
-
+      if (
+        !image ||
+        !productName ||
+        !price ||
+        !unit ||
+        !category ||
+        !description ||
+        !quantity
+      ) {
+        setError("Please fill all the fields");
+        setLoading(false);
+        return;
+      }
       const productDetails: ProductDetails = {
         productName,
         price,
@@ -126,6 +140,7 @@ export default function Sell() {
       if (response.ok) {
         setLoading(false);
         alert("Product listed successfully!");
+        navigate("");
       } else {
         setError(data.message || "Product listing failed");
         setLoading(false);
